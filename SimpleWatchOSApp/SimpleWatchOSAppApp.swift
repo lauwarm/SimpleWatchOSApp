e6c853bb93f10e1c8eff473e30692a50c9b99105
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-02-02 10:33:09 +0100

    Initial Commit

---
+++ SimpleWatchOSApp/SimpleWatchOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWatchOSAppApp.swift
+//  SimpleWatchOSApp
+//
+//  Created by Fabian on 02.02.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWatchOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
