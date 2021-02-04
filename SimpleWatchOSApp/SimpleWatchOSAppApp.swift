50518156e39e52bc8c79e132be7ccee157795ed2
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-02-04 21:54:17 +0100

    create a custom notification interface 

--- SimpleWatchOSApp/SimpleWatchOSAppApp.swift
+++ SimpleWatchOSApp/SimpleWatchOSAppApp.swift
@@ -16,5 +16,9 @@ struct SimpleWatchOSAppApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
