2b3c18029cd1d4c97c9978849f378e109c3d9316
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-02-04 20:48:12 +0100

    share files between targets

---
+++ WatchOSApp Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchOSApp Extension
+//
+//  Created by Fabian on 02.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchOSApp Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchOSApp Extension
+//
+//  Created by Fabian on 02.02.21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
